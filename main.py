fa4d616b9b1eeb49f3af0f84f97c4cf34a6ae4b3
N <zkac424@live.rhul.ac.uk>
2023-02-04 16:27:12 +0000

    Initialised pygame and created a screen

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-# This will be the main file where the game will be run from
-
 from tkinter import *
+import pygame
+
+
+# initialise pygame
+pygame.init()
+# creating the screen
+screen = pygame.display.set_mode((1000, 750))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+class Instruction:
+    pass
